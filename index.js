48d60406b84d97473ebe6e457296e172dfa1f2c4
rgrannell1 <r.grannell2@gmail.com>
2020-04-04 20:27:13 +0100

    test

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const MinecraftQuery = require('minecraft-query')
 const psList = require('ps-list')
 const dayjs = require('dayjs')
 
+const {exec} = require('child_process')
+
 const constants = {
   timers: {
     shutdownMs: 20 * 60 * 1000
-  }
+  },
+  host: '0.0.0.0',
+  port: 25565
 }
 
 const timestamp = () => {
@@ -41,10 +45,10 @@ const parseData = data => {
   }
 }
 
-const fetchData = async () => {
+const fetchServerData = async () => {
   const query = new MinecraftQuery({
-    host: '0.0.0.0',
-    port: 25565,
+    host: constants.host,
+    port: constants.port,
     timeout: 10000
   })
 
@@ -63,35 +67,52 @@ const state = {
   lastActive: Date.now()
 }
 
+const handleInactiveState = state => {
+  let diffMs = now - state.lastActive
+  let remainingSeconds = (constants.timers.shutdownMs - diffMs) / 1000
+
+  console.log(`${timestamp()}: server inactive; shutting down in ${remainingSeconds} seconds`)
+
+  if (diffMs > constants.timers.shutdownMs) {
+    console.log(`${timestamp()}: server inactive; shutting down NOW`)
+
+    exec('shutdown now', (err, stdout, stderr) {
+      if (err) {
+        console.error(err)
+        process.exit(1)
+      }
+
+      process.stdout.pipe(stdout)
+      process.stderr.pipe(stderr)
+    })
+  }
+}
+
 const main = async () => {
   console.log('polling local server for user-counts')
 
+  // -- todo check if server is online
   const tasks = await psList()
   console.log(tasks.map(d => d.name))
 
-  let serverData = await fetchData()
+  let serverData = await fetchServerData()
   let now = Date.now()
 
   if (!serverData) {
+    // -- could not retrieve query data
 
     console.log(`${timestamp()}: failed to connect to server`)
-
+    handleInactiveState(state)
 
   } else if (serverData.online && serverData.online > 0) {
+    // -- server is active
 
     state.lastActive = Date.now()
     console.log(`${timestamp()}: server active with ${serverData.online} players`)
 
   } else {
-
-    let diffMs = now - state.lastActive
-    let remainingSeconds = (constants.timers.shutdownMs - diffMs) / 1000
-
-    console.log(`${timestamp()}: server inactive; shutting down in ${remainingSeconds} seconds`)
-
-    if (diffMs > constants.timers.shutdownMs) {
-      console.log(`${timestamp()}: server inactive; shutting down NOW`)
-    }
+    // -- server is inactive
+    handleInactiveState(state)
   }
 
   setTimeout(() => main(), 15000)
