8b744d470492cef0a45f8ca9a257fc47865aa84b
rgrannell1 <r.grannell2@gmail.com>
2020-05-17 03:05:26 +0100

    Added pause option & deprovisions as root

--- src/ansible/files/server-utils/commands/deprovision.js
+++ src/ansible/files/server-utils/commands/deprovision.js
@@ -73,8 +73,21 @@ const state = {
   lastActive: Date.now()
 }
 
-const beginShutdown = () => {
+const beginShutdown = async () => {
+  const credentials = btoa(`${process.env.BASIC_AUTH_USER}:${process.env.BASIC_AUTH_PASS}`)
 
+  const pauseRes = await fetch(`${constants.urls.api}/pause`, {
+    method: 'POST',
+    headers: {
+      Authorization: credentials
+    }
+  })
+
+  console.log(credentials)
+  console.log('xxx')
+
+  // -- todo; shutdown minecraft gracefully
+  await execa('poweroff')
 }
 
 const handleInactiveState = async state => {
@@ -83,7 +96,11 @@ const handleInactiveState = async state => {
   let diffMs = now - state.lastActive
   let remainingSeconds = (constants.timers.shutdownMs - diffMs) / 1000
 
-  console.log(`${timestamp()}: server inactive; shutting down in ${Math.ceil(remainingSeconds / 60)} minutes`)
+  const shutdownTime = remainingSeconds > 0
+    ? Math.ceil(remainingSeconds / 60)
+    : 'now'
+
+  console.log(`${timestamp()}: server inactive; shutting down in ${shutdownTime} minutes`)
 
   if (diffMs > constants.timers.shutdownMs) {
     console.log(`${timestamp()}: server inactive; triggering deprovisioning`)
@@ -92,12 +109,8 @@ const handleInactiveState = async state => {
   }
 }
 
-command.task = async () => {
-  process.getuid()
-
+const main = async () => {
   console.log('polling local server for user-counts')
-
-  const tasks = await psList()
 
   let serverData = await fetchServerData()
 
@@ -116,7 +129,13 @@ command.task = async () => {
     await handleInactiveState(state)
   }
 
-  setTimeout(() => main(), 15000)
+  setTimeout(async () => {
+    await main()
+  }, 15000)
+}
+
+command.task = async () => {
+  await main()
 }
 
 module.exports = command
